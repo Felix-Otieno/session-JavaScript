bfd37a36b7babc93d76e61421b3df5c488d073c1
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-07-10 13:03:46 +0300

    add changes

--- dom.js
+++ dom.js
@@ -1,2 +1,26 @@
 const test = document.getElementById("test");
 test.innerHTML = "My name is Afeloh"; 
+
+//creating new elements
+
+const newItem = document.createElement("h1");
+const headingTitle = document.createTextNode("This is a new element");
+
+
+newItem.appendChild(headingTitle);
+
+const printToDom = document.getElementById("newElement");
+printToDom.appendChild(newItem);
+
+//ASSIGNMENT
+// - Create new element that prints out your favorite dish, pets, colors
+// - Make sure to push this code in Github  
+
+
+const food = document.createElement("h2");
+const foodtitle = document.createTextNode("My favorite: biriani, bull dog and blue");
+
+food.appendChild(foodtitle);
+
+const printToDom2 = document.getElementById("food");
+printToDom2.appendChild(food);
