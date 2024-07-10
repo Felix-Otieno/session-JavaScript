bb7a16856df9005238f4544fb4b3a50b20ad8b97
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-07-10 10:48:53 +0300

    add functions

---
+++ function.js
@@ -0,0 +1,41 @@
+function greet() {
+    // block of code
+    console.log("JavaScriptvbu");
+}
+
+greet();
+
+const greet2 = (fName, lName, gender, age) => {
+    
+console.log(fName, lName, gender, age);
+
+}; 
+
+greet2("Afeloh", "Fellas", "male", 20);
+
+
+// function displaysum () {
+//     let a = 20;
+//     let b = 30;
+//     let sum = a + b;
+
+//     console.log(sum);
+// }
+
+// displaysum();
+
+// function add( a, b) {
+//     return a + b;
+// }
+
+function displaysum(a, b) {
+    //let sum = a + b
+    console.log(a + b);
+
+    
+}
+
+displaysum(10, 30);
+
+
+
